3079469ef1ad2c6767add6b1aad8ad2fb7ab9187
SAHIL <sahilsayani7@gmail.com>
2022-07-18 23:16:06 +0530

    first commit

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,11 @@
+
+const FeedbackItem = () => {
+  return (
+    <div className="card">FeedbackItem
+    <div className="num-display">10</div>
+    <div className="text-display">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+    </div>
+  )
+}
+
+export default FeedbackItem
